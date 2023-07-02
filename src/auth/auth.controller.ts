2cb8441660bebc1c5c167ba4ae360151b8489be5
Zanuda1337 <dsnake456@gmail.com>
2023-07-02 16:55:44 +0300

    fixed cookie

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
 import {
   Body,
   Controller,
-  Delete, Get,
+  Delete,
+  Get,
   HttpCode,
   HttpStatus,
   Post,
@@ -14,25 +15,29 @@ import {
   ApiBearerAuth,
   ApiConflictResponse,
   ApiCookieAuth,
-  ApiCreatedResponse, ApiOperation, ApiResponse,
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiResponse,
   ApiTags,
 } from '@nestjs/swagger';
 import { RtGuard } from './common/guards';
-import {
-  Cookies,
-  GetCurrentUserId,
-  Public,
-} from './common/decorators';
+import { Cookies, GetCurrentUserId, Public } from './common/decorators';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { LoginUserDto } from '../users/dto/login-user.dto';
-import { Response } from 'express';
+import { CookieOptions, Response } from 'express';
 import { User } from '../users/users.model';
 
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+  private cookieOptions: CookieOptions = {
+    httpOnly: true,
+    sameSite: 'none',
+    secure: true,
+    maxAge: +process.env.RT_EXPIRE,
+  }
 
   @HttpCode(200)
   @ApiOperation({ summary: 'Получить текущего пользователя' })
@@ -45,11 +50,19 @@ export class AuthController {
 
   @Post('/registration')
   @Public()
-  @ApiConflictResponse({ status: HttpStatus.CONFLICT, description: 'Пользователь с таким Email уже существует', })
-  @ApiBadRequestResponse({ status: HttpStatus.BAD_REQUEST, description: 'Некорректные данные', })
-  @ApiCreatedResponse({ status: HttpStatus.CREATED, description: 'Пользователь создан', })
+  @ApiConflictResponse({
+    status: HttpStatus.CONFLICT,
+    description: 'Пользователь с таким Email уже существует',
+  })
+  @ApiBadRequestResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'Некорректные данные',
+  })
+  @ApiCreatedResponse({
+    status: HttpStatus.CREATED,
+    description: 'Пользователь создан',
+  })
   @HttpCode(HttpStatus.CREATED)
-
   async registration(
     @Body() userDto: CreateUserDto,
     @Res({ passthrough: true }) res: Response,
@@ -57,7 +70,7 @@ export class AuthController {
     const { access_token, refresh_token } = await this.authService.registration(
       userDto,
     );
-    res.cookie('refresh_token', refresh_token, { httpOnly: false });
+    res.cookie('refresh_token', refresh_token, this.cookieOptions);
     return { access_token };
   }
 
@@ -71,22 +84,26 @@ export class AuthController {
     const { access_token, refresh_token } = await this.authService.login(
       userDto,
     );
-    res.cookie('refresh_token', refresh_token, { httpOnly: false });
+    res.cookie('refresh_token', refresh_token, this.cookieOptions);
     return { access_token };
   }
 
   @Put('/refresh')
-  @ApiOperation({ summary: 'Обновить refresh_token и получить новую пару токенов' })
+  @ApiOperation({
+    summary: 'Обновить refresh_token и получить новую пару токенов',
+  })
   @Public()
   @UseGuards(RtGuard)
   @ApiCookieAuth('refresh_token')
   @HttpCode(HttpStatus.OK)
   async refresh(
     @Cookies('refresh_token') refreshToken: string,
-    @Res({ passthrough: true }) res: Response
+    @Res({ passthrough: true }) res: Response,
   ) {
-    const { access_token, refresh_token } = await this.authService.refresh(refreshToken);
-    res.cookie('refresh_token', refresh_token, { httpOnly: false });
+    const { access_token, refresh_token } = await this.authService.refresh(
+      refreshToken,
+    );
+    res.cookie('refresh_token', refresh_token, this.cookieOptions);
     return { access_token };
   }
 
@@ -94,7 +111,7 @@ export class AuthController {
   @ApiBearerAuth('access_token')
   @HttpCode(HttpStatus.OK)
   logout(@GetCurrentUserId() userId) {
-    console.log(userId)
+    console.log(userId);
     return this.authService.logout(userId);
   }
 }
